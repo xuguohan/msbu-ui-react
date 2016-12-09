7f7a7c1a31999b3c5374c09474bfa1f5191dfe7a
langjun <leo.langjun@gmail.com>
2016-12-09 18:27:23 +0800

    Update iconfont for msbu && release 0.1.9

--- components/icon/index.web.tsx
+++ components/icon/index.web.tsx
@@ -12,5 +12,5 @@ export interface IconProps {
 
 export default (props: IconProps) => {
   const { type, className = '' } = props;
-  return <i {...props} className={`${className} anticon anticon-${type.replace(/-/g, '')}`.trim()} />;
+  return <i {...props} className={`${className} msbuicon msbuicon-${type}`.trim()} />;
 };
